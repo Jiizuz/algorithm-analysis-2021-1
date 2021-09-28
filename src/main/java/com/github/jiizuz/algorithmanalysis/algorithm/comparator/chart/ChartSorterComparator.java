90366e8cf0f4c93e07d818f3ca52605bc6add3ee
Jiizuz <masterchack92@hotmail.com>
2021-09-28 10:05:37 -0500

    reformat(comparator): Encapsulation of Chart comparators.

--- src/main/java/com/github/jiizuz/algorithmanalysis/algorithm/comparator/chart/ChartSorterComparator.java
+++ src/main/java/com/github/jiizuz/algorithmanalysis/algorithm/comparator/chart/ChartSorterComparator.java
@@ -1,8 +1,9 @@
-package com.github.jiizuz.algorithmanalysis.algorithm.fibonacci;
+package com.github.jiizuz.algorithmanalysis.algorithm.comparator.chart;
 
+import com.github.jiizuz.algorithmanalysis.algorithm.sort.Sorter;
+import com.github.jiizuz.algorithmanalysis.algorithm.sort.SorterComparator;
 import com.github.jiizuz.algorithmanalysis.benchmark.Benchmark;
 import com.github.jiizuz.algorithmanalysis.benchmark.TimeResults;
-import it.unimi.dsi.fastutil.ints.Int2IntFunction;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NonNull;
@@ -14,33 +15,39 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 import java.util.List;
 import java.util.RandomAccess;
+import java.util.function.IntFunction;
 
 /**
- * {@link FibonacciComparator} that uses a {@link JFreeChart} to
+ * {@link SorterComparator} that uses a {@link JFreeChart} to
  * display a comparison in a XY Line Chart.
  *
- * <p>This implementation tests the functions based on a specified
+ * <p>This implementation tests the sorters based on a specified
  * amount of times on {@link #tests}.
+ *
+ * <p>The used array to send to the sorters is generated with the
+ * specified {@link #arrGenerator} sending it the number of the
+ * current tests. The same array is sent to all the sorters in
+ * order to make a valid tests with the same data.
  *
  * <p>A {@link ChartFrame} is used to pack and display the chart.
  *
  * @author <a href="mailto:masterchack92@hotmail.com">Jiizuz</a>
- * @see com.github.jiizuz.algorithmanalysis.algorithm.fibonacci.FibonacciComparator
+ * @see com.github.jiizuz.algorithmanalysis.algorithm.sort.SorterComparator
  * @since 1.0
  */
 @Builder
 @AllArgsConstructor
-public class ChartFibonacciComparator implements FibonacciComparator {
+public class ChartSorterComparator implements SorterComparator {
 
     /**
      * Default title to set on the <tt>Chart</tt>.
      */
-    private static final String CHART_TITLE = "CPU Time / Fibonacci number";
+    private static final String CHART_TITLE = "CPU Time / array length";
 
     /**
      * Default label for the <tt>X axis</tt> on the chart.
      */
-    private static final String X_AXIS_LABEL = "Fibonacci number";
+    private static final String X_AXIS_LABEL = "array length";
 
     /**
      * Default label for the <tt>Y axis</tt> on the chart.
@@ -50,7 +57,7 @@ public class ChartFibonacciComparator implements FibonacciComparator {
     /**
      * Default title to set on the <tt>Frame</tt>.
      */
-    private static final String FRAME_TITLE = "Fibonacci Comparator";
+    private static final String FRAME_TITLE = "Sorters Comparator";
 
     /**
      * Default amount of tests to made if not specified.
@@ -58,13 +65,12 @@ public class ChartFibonacciComparator implements FibonacciComparator {
     private static final int DEFAULT_TESTS = 100;
 
     /**
-     * {@link Int2IntFunction} to retrieve the amount of fibonacci numbers to calculate
-     * based on the current test made.
+     * {@link IntFunction} to generate arrays to sort.
      *
      * <p>The given input is the number of the test.
      */
     @NonNull
-    private final Int2IntFunction numberSpecifier;
+    private final IntFunction<long[]> arrGenerator;
 
     /**
      * {@link Benchmark} to use on the accumulation.
@@ -103,7 +109,7 @@ public class ChartFibonacciComparator implements FibonacciComparator {
     private final String frameTitle = FRAME_TITLE;
 
     /**
-     * {@link XYSeriesCollection} to accumulate on the series of the Fibonacci numbers.
+     * {@link XYSeriesCollection} to accumulate on the series of the sorters.
      */
     private final XYSeriesCollection seriesCollection = new XYSeriesCollection();
 
@@ -111,22 +117,24 @@ public class ChartFibonacciComparator implements FibonacciComparator {
      * {@inheritDoc}
      */
     @Override
-    public <T extends List<Fibonacci> & RandomAccess> void accumulate(final @NonNull T functions) {
-        final XYSeries[] xySeries = new XYSeries[functions.size()];
+    public <T extends List<Sorter> & RandomAccess> void accumulate(final @NonNull T sorters) {
+        final XYSeries[] xySeries = new XYSeries[ sorters.size() ];
 
-        for (int i = 0, n = functions.size(); i < n; ++i) {
-            final XYSeries series = new XYSeries(functions.get(i).getClass().getSimpleName());
-            seriesCollection.addSeries(series);
+        for ( int i = 0, n = sorters.size(); i < n; ++i )
+        {
+            final XYSeries series = new XYSeries( sorters.get( i ).getClass().getSimpleName() );
+            seriesCollection.addSeries( series );
             xySeries[i] = series;
         }
 
-        for (int i = 1; i <= tests; ++i) {
-            final int fibonacciOf = numberSpecifier.apply(i);
+        for ( int i = 1; i <= tests; ++i )
+        {
+            final long[] toSort = arrGenerator.apply( i );
 
-            for (int j = 0, n = functions.size(); j < n; ++j) {
-                final TimeResults results = benchmark.test(functions.get(j), () -> fibonacciOf);
+            for ( int j = 0, n = sorters.size(); j < n; ++j ) {
+                final TimeResults results = benchmark.test( sorters.get( j ), toSort::clone );
 
-                xySeries[j].add(i, results.getTimes().intStream().average().orElse(0D));
+                xySeries[j].add( i, results.getTimes().intStream().average().orElse( 0D ) );
 
                 results.dump();
             }
@@ -138,9 +146,9 @@ public class ChartFibonacciComparator implements FibonacciComparator {
      */
     @Override
     public void displayData() {
-        final JFreeChart chart = ChartFactory.createXYLineChart(chartTitle, xAxisLabel, yAxisLabel, seriesCollection);
-        final ChartFrame frame = new ChartFrame(frameTitle, chart);
+        final JFreeChart chart = ChartFactory.createXYLineChart( chartTitle, xAxisLabel, yAxisLabel, seriesCollection );
+        final ChartFrame frame = new ChartFrame( frameTitle, chart );
         frame.pack();
-        frame.setVisible(true);
+        frame.setVisible( true );
     }
 }
